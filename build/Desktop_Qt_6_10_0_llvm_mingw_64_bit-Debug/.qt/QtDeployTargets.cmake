ddb9928891a43ad667e2ffef8f3d0c45c5f80a3b
IgorK97 <kt.wert.74@gmail.com>
2025-02-22 19:22:57 +0300

    kkk

---
+++ build/Desktop_Qt_6_10_0_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_untitledProject_FILE C:/Users/IgorA/source/repos/MyProjects/QtProjects/untitledProject/build/Desktop_Qt_6_10_0_llvm_mingw_64_bit-Debug/untitledProject.exe)
+set(__QT_DEPLOY_TARGET_untitledProject_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_untitledProject_RUNTIME_DLLS C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Core.dll)
